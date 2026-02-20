6329d26a0d602b32d5a4e40e9befb1607eac6bd2
ims2002 <imsmorsell2002@gmail.com>
2026-02-21 00:14:28 +0100

    Cambios en las funciones de limpieza

--- src/cleaning.py
+++ src/cleaning.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 
 def clean(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+    df['Category'] = df['Category'].str.upper()
+
+    df['Installs'] = df['Installs'].str.replace('+', '', regex=False)
+    df['Installs'] = df['Installs'].str.replace(',', '', regex=False)
+    df['Installs'] = df['Installs'].str.replace('installs', '', regex=False)
+    df['Installs'] = df['Installs'].str.strip()
+
+    return df
